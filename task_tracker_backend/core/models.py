64e0e5e12ae58aa1863654dac90bf8f2c2a97bf1
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2024-01-08 23:09:02 -0300

    feat: created_date field on Task model

--- task_tracker_backend/core/models.py
+++ task_tracker_backend/core/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django_fsm import FSMField, transition
 
@@ -13,13 +15,22 @@ class Task(models.Model):
         (COMPLETED_STATE, COMPLETED_STATE),
     ]
 
+    created_date = models.DateTimeField()
     name = models.CharField(max_length=40, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     estimated = models.DurationField()
     state = FSMField(choices=STATE_CHOICES, default=PLANNED_STATE)
 
     def __str__(self):
-        return self.name
+        return self.name.capitalize()
+
+    def save(
+        self,
+        *args,
+        **kwargs
+    ):
+        self.created_date = datetime.datetime.now()
+        return super().save(*args, **kwargs)
 
     @transition(field=state, source=PLANNED_STATE, target=IN_PROGRESS_STATE)
     def progress(self):
