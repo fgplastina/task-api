1ee22a82b61539e24ebf7927564972bf882a4df8
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2024-01-08 21:22:43 -0300

    refac: made a more simple solution with transitions from django_fsm

--- task_tracker_backend/core/models.py
+++ task_tracker_backend/core/models.py
@@ -1,55 +1,30 @@
 from django.db import models
-
-# Create your models here.
-
-class TaskState:
-    def transition_to(self, task):
-        raise Exception("Not implement yet")
-
-
-    def __str__(self):
-        pass
-
-class PlannedState(TaskState):
-    def transition_to(self, task):
-        task.state = InProgressState()
-
-    def __str__(self):
-        return "Planned"
-
-class InProgressState(TaskState):
-    def transition_to(self, task):
-        task.state = CompletedState()
-
-    def __str__(self):
-        return "In Progress"
-
-class CompletedState(TaskState):
-    def transition_to(self, task):
-        raise Exception(f"task {task} in {task.COMPLETED_STATE} state cannot be transitioned to a different state.")
-
-
-    def __str__(self):
-        return "Completed"
+from django_fsm import FSMField, transition
 
 
 class Task(models.Model):
     PLANNED_STATE = "planned"
-    IN_PROGRESS_STATE = "in_progress"
+    IN_PROGRESS_STATE = "in progress"
     COMPLETED_STATE = "completed"
 
     STATE_CHOICES = [
-        (PLANNED_STATE, "Planned"),
-        (IN_PROGRESS_STATE, "In Progress"),
-        (COMPLETED_STATE, "Completed"),
+        (PLANNED_STATE, PLANNED_STATE),
+        (IN_PROGRESS_STATE, IN_PROGRESS_STATE),
+        (COMPLETED_STATE, COMPLETED_STATE),
     ]
 
     name = models.CharField(max_length=40, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     estimated = models.DurationField()
-    state = models.CharField(
-        max_length=20, choices=STATE_CHOICES, default="planned"
-    )
+    state = FSMField(choices=STATE_CHOICES, default=PLANNED_STATE)
 
     def __str__(self):
         return self.name
+
+    @transition(field=state, source=PLANNED_STATE, target=IN_PROGRESS_STATE)
+    def progress(self):
+        pass
+
+    @transition(field=state, source=IN_PROGRESS_STATE, target=COMPLETED_STATE)
+    def complete(self):
+        pass
